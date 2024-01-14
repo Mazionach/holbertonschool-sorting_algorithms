2578594eea2d9c8daa64c91c4efb23fed287020f
Mazionach <mazafesio25@gmail.com>
2024-01-13 18:19:05 -0800

    Fixed jumbling of unordered part of array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t sort, i;
-	int tmp, sorted;
+	int tmp, sorted, min;
 
 	for (sort = 0; sort < size; sort++)
 	{
+		min = array[sort];
 		sorted = 0;
 		for (i = sort + 1; i < size; i++)
 		{
-			if (array[i] < array[sort])
+			if (array[i] < min)
 			{
-				tmp = array[sort];
-				array[sort] = array[i];
-				array[i] = tmp;
-				sorted = 1;
+				min = array[i];
+				sorted = i;
 			}
 		}
-		if (sorted == 1)
+		if (sorted)
+		{
+			tmp = array[sort];
+			array[sort] = array[sorted];
+			array[sorted] = tmp;
 			print_array(array, size);
+		}
 	}
 }
